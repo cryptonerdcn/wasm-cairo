fcd9d132879da6ba63c9a00cc32beef83e77d30e
cryptonerdcn <cryptonerdcn@gmail.com>
2023-07-19 09:57:16 +0900

    Update: use wee_alloc.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const path = require('path');
 
 //app.use(express.static(path.join(__dirname, 'pkg')));
